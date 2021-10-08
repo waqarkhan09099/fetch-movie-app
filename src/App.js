6d6b4c7a68088f9420bb73f0015a629d23de280c
Muhammad Waqar Khan <63258252+waqarkhan09099@users.noreply.github.com>
2021-10-08 03:57:25 -0700

    Error Checkup
    we  use try and catch to check error in fetched data. if we get error it will show in screen

--- src/App.js
+++ src/App.js
@@ -8,26 +8,37 @@ import CustomForm from './Components/Custom Movies/MovieForm'
 function App() {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
+  const [error, setError]=useState(null)
   const data1=[]
   async function fetchDataHandler() {
     // async function fetchData() {
     setLoading(false)
+    setError(null)
     // const response = await fetch(`https://swapi.dev/api/films/`)
-    const response = await fetch(`https://my-react-project-fd7ea-default-rtdb.firebaseio.com/movies.json`)
-    const data = await response.json();
-    // setMovies(data.results);
-    // we apply loop for get data form keys and are random from firebase
-    for(let key in data){
-      data1.push({
-        title:data[key].title,
-        ReleaseDate:data[key].ReleaseDate,
-        director:data[key].director,
-        about:data[key].about,
-      })
+    try{
+      
+      const response = await fetch(`https://my-react-project-fd7ea-default-rtdb.firebaseio.com/movies.json`)
+      const data = await response.json();
+      // setMovies(data.results);
+      // we apply loop for get data form keys and are random from firebase
+      if(!response.ok){
+        throw new Error('Something went Wrong');
+      }
+      for(let key in data){
+        data1.push({
+          title:data[key].title,
+          ReleaseDate:data[key].ReleaseDate,
+          director:data[key].director,
+          about:data[key].about,
+        })
+      }
+      setMovies(data1);
+      // console.log(data);
+      // }
     }
-    setMovies(data1);
-    // console.log(data);
-    // }
+    catch(error){
+      setError(error.message);
+    }
     setLoading(true)
     // fetchData();
   }
@@ -50,8 +61,9 @@ function App() {
         <ButtonUi onClick={fetchDataHandler}>Fetch Data</ButtonUi>
       </Wrapper>
       <Wrapper>
-        {loading && movies.length === 0 && <p>Movies not found</p>}
+        {loading && movies.length === 0 && !error && <p>Movies not found</p>}
         {!loading && movies.length === 0 && <p>Loading...</p>}
+        {!loading && error && <p>{error}</p>}
         {movies.length > 0 && <Movie data={movies} />}
       </Wrapper>
     </>
