5e7cf587473218d76cfffd770d4615f73af30833
Muhammad Waqar Khan <63258252+waqarkhan09099@users.noreply.github.com>
2021-11-23 14:19:13 +0500

    api form swapi

--- src/App.js
+++ src/App.js
@@ -14,24 +14,27 @@ function App() {
     // async function fetchData() {
     setLoading(false)
     setError(null)
-    // const response = await fetch(`https://swapi.dev/api/films/`)
+    const response = await fetch(`https://swapi.dev/api/films/`)
     try{
       
-      const response = await fetch(`https://my-react-project-fd7ea-default-rtdb.firebaseio.com/movies.json`)
+      // const response = await fetch(`https://my-react-project-fd7ea-default-rtdb.firebaseio.com/movies.json`)
       const data = await response.json();
+      console.log(data)
       // setMovies(data.results);
       // we apply loop for get data form keys and are random from firebase
       if(!response.ok){
         throw new Error('Something went Wrong');
       }
-      for(let key in data){
+
+      for(let key in data.results){
         data1.push({
-          title:data[key].title,
-          ReleaseDate:data[key].ReleaseDate,
-          director:data[key].director,
-          about:data[key].about,
+          title:data.results[key].title,
+          ReleaseDate:data.results[key].release_date,
+          director:data.results[key].director,
+          about:data.results[key].opening_crawl,
         })
       }
+      console.log(data1)
       setMovies(data1);
       // console.log(data);
       // }
@@ -44,6 +47,7 @@ function App() {
   }
 
   async function getDataFunc(data){
+    alert("sorry due to firebase unauthorized. information not post at database")
     const response=await fetch(`https://my-react-project-fd7ea-default-rtdb.firebaseio.com/movies.json`,{
       method:"POST",
       body:JSON.stringify(data),
@@ -59,6 +63,7 @@ function App() {
       <CustomForm Data={getDataFunc}/>
       <Wrapper>
         <ButtonUi onClick={fetchDataHandler}>Fetch Data</ButtonUi>
+        {loading && movies.length === 0 && <p>Click Fetch button we will serve dummy films.</p>}
       </Wrapper>
       <Wrapper>
         {loading && movies.length === 0 && !error && <p>Movies not found</p>}
